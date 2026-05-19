95f32b2d7b1e7565b95001bb4e32463f9cdd0596
melulekigabriel-wq <dlaminimelulekig@gmail.com>
2026-05-19 06:24:11 +0200

    Create smc.py

---
+++ smc.py
@@ -0,0 +1,13 @@
+def detect_order_block():
+
+    return "Bullish Order Block"
+
+
+def detect_liquidity():
+
+    return "Liquidity Sweep Detected"
+
+
+def detect_fvg():
+
+    return "Fair Value Gap Found"
